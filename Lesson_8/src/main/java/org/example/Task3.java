7e6b1effaa93d679030048cf659b78b8d236d1f6
Ilnaz Galiev <qazxswedc20063006@gmail.com>
2025-05-31 10:14:02 +0300

    hw5

--- Lesson_8/src/main/java/org/example/Task3.java
+++ Lesson_8/src/main/java/org/example/Task3.java
@@ -7,25 +7,21 @@ public class Task3 {
             return x * y;
         }
 
-        // Найдём наибольшее количество битов
         int n = Math.max(Integer.highestOneBit(x), Integer.highestOneBit(y));
         int half = Integer.toBinaryString(n).length() / 2;
 
-        // Разделим x на a и b, y на c и d
-        int highMask = ~((1 << half) - 1); // маска для верхней части
-        int lowMask = (1 << half) - 1;     // маска для нижней части
+        int highMask = ~((1 << half) - 1);
+        int lowMask = (1 << half) - 1;
 
         int a = (x & highMask) >>> half;
         int b = x & lowMask;
         int c = (y & highMask) >>> half;
         int d = y & lowMask;
 
-        // Рекурсивные вызовы
         int ac = karatsuba(a, c);
         int bd = karatsuba(b, d);
         int abcd = karatsuba(a + b, c + d);
 
-        // Результат: ac * 2^(2*half) + (abcd - ac - bd) * 2^half + bd
         int result = (ac << (2 * half)) + ((abcd - ac - bd) << half) + bd;
         return result;
     }
@@ -36,9 +32,6 @@ public class Task3 {
 
         int result = karatsuba(x, y);
 
-        System.out.println("Binary:");
-        System.out.println(Integer.toBinaryString(x) + " * " + Integer.toBinaryString(y)
-                + " = " + Integer.toBinaryString(result));
         System.out.println(x + " * " + y + " = " + result);
     }
 }
